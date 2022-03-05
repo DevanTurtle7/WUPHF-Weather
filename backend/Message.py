0f9e0189762afb6f3b81b0e48308337f5538c281
Ethan Iannicelli <etiannicelli@gmail.com>
2022-03-05 17:46:19 -0500

    added api key for openweather

--- backend/Message.py
+++ backend/Message.py
@@ -3,13 +3,14 @@ from flask import request
 import requests
 from get_message import make_message
 
+key = '0d95e14a5b884b1441e2341498d9b676'
+
 
 class Message(Resource):
     def post(self):
         lat = request.json['latitude']
         lon = request.json['longitude']
-        key = request.json['key']
-        url = "pro.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=imperial" % (lat, lon, key)
+        url = "api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=imperial" % (lat, lon, key)
 
         response = requests.get(url)
         return make_message(response)
