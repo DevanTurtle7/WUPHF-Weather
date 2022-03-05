f742283819c5bec1038591a88eeeaf2952a1d932
Ethan Iannicelli <etiannicelli@gmail.com>
2022-03-05 15:29:05 -0500

    created server

--- backend/Message.py
+++ backend/Message.py
@@ -1,10 +1,10 @@
-from flask_restful import Resource, Api
+from flask_restful import Resource
 from flask import request
 import requests
-import json
 from get_message import make_message
 
-class Weather(Resource):
+
+class Message(Resource):
     def post(self):
         lat = request.json['latitude']
         lon = request.json['longitude']
@@ -12,6 +12,5 @@ class Weather(Resource):
         url = "pro.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=imperial" % (lat, lon, key)
 
         response = requests.get(url)
-        data = json.loads(response.text)
         return make_message(response)
 
