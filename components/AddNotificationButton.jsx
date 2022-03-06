53c0f20818a05d5ce1255186b527d50398f387ef
Devan Kavalchek <devankav7@gmail.com>
2022-03-05 22:51:29 -0500

    updated modal

--- components/AddNotificationButton.jsx
+++ components/AddNotificationButton.jsx
@@ -1,25 +1,37 @@
 import { View, Text, StyleSheet, Pressable } from 'react-native'
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Modal from "react-native-modal";
 
+import Button from './Button';
+import TextButton from './TextButton';
+
 function AddNotificationButton(props) {
+    const [modalOpen, setModalOpen] = useState(false)
 
-    const onClick = () => {
+    const openModal = () => {
+        setModalOpen(true)
+    }
 
+    const closeModal = () => {
+        setModalOpen(false)
     }
 
     return (
         <Fragment>
-            <Pressable style={styles.row} onPress={onClick}>
+            <Pressable style={styles.row} onPress={openModal}>
                 <Icon name="alarm-plus" size={30} style={styles.icon} />
                 <Text style={styles.label}>Add new notification</Text>
-
             </Pressable>
 
-            <Modal isVisible={false} style={styles.modal}>
+            <Modal isVisible={modalOpen} style={styles.modal}>
                 <View style={styles.modalBody}>
                     <Text>His</Text>
+
+                    <View style={styles.footer}>
+                        <TextButton title="Cancel" onPress={closeModal}/>
+                        <Button title="Add" />
+                    </View>
                 </View>
             </Modal>
         </Fragment>
@@ -48,7 +60,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: "#fff",
-        width: '80%'
+        width: '90%'
+    },
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        paddingHorizontal: 10,
+        paddingBottom: 10
     }
 })
 
